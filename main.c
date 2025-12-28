01e4a8d30338544a88be7e53f0b905d3786d0a11
Marcs <codemarcs@gmail.com>
2025-12-28 12:54:42 -0300

    Refactor BMI calculation and add age parameter
    A table has been added for more accurate BMI classification.

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    double minNormal;
+    double maxNormal;
+    double obesityI;
+    double obesityII;
+} BmiTable;
 
 /**
  * Calculates and prints BMI and ideal weight range based on weight and height.
  */
-void calculateBMI(double weight, double height);
+double calculateBMI(double weight, double height, double minNormal, double maxNormal);
+/** 
+ * Classify BMI.
+ */
+void classifyBMI(double bmi, double minNormal, double maxNormal, double obesityI, double obesityII); 
+/**
+ * Load the table.
+ */
+BmiTable loadTable(double age);
 /**
  * Clears the terminal screen depending on the OS.
  */
@@ -14,7 +30,6 @@ void clearScreen();
  */
 void printBanner();
 
-
 #define BOLD    "\033[1m"
 #define RESET   "\033[0m"
 
@@ -23,13 +38,6 @@ void printBanner();
 #define GREEN   "\033[32m"
 #define YELLOW  "\033[33m"
 
-// BMI thresholds based on WHO
-#define BMI_MIN_NORMAL    18.5
-#define BMI_MAX_NORMAL    24.9
-
-#define BMI_MIN_OB1       30.0
-#define BMI_MIN_OB2       35.0
-
 /**
  * Program entry point: prompts for weight and height, then shows BMI info.
  */
@@ -37,11 +45,11 @@ int main(void)
 {
     // This causes the text to be printed immediately on the terminal
     setbuf(stdout, NULL);
-    
+
     clearScreen();
     printBanner();
 
-    double weight, height;
+    double weight, height, age;
 
     // Prompt for weight
     printf(GREEN "Your weight (kg): " RESET);
@@ -51,49 +59,119 @@ int main(void)
     printf(GREEN "Your height (m): " RESET);
     scanf("%lf", &height);
 
+    // Prompt for age
+    printf(GREEN "Your age (y): " RESET);
+    scanf("%lf", &age);
+
     printf("\n" RESET);
 
-    // Calculate BMI and print results
-    calculateBMI(weight, height);
+    // Creates variables from the table
+    BmiTable table = loadTable(age);
+
+    // Calculate BMI
+    double bmi = calculateBMI(weight, height, table.minNormal, table.maxNormal);
+
+    // Classifies the BMI
+    classifyBMI(bmi, table.minNormal, table.maxNormal, table.obesityI, table.obesityII);
 
     return 0;
 }
 
 /**
- * Calculates and prints BMI and ideal weight range based on weight and height.
+ * Create variables from the table for more precise results.
  */
-void calculateBMI(double weight, double height)
+BmiTable loadTable(double age)
+{
+    char line[256];
+    int counter = 1;
+    BmiTable bmiTable;
+    FILE *table = fopen("./assets/table.csv", "r");
+
+    double minNormal = 0;
+    double maxNormal = 0;
+    double obesityI = 0;
+    double obesityII = 0;
+
+    // Check if it has been loaded  
+    if (table == NULL)
+    {
+        printf("Error in open the archive");
+        return bmiTable;
+    }
+
+    // According to the Brazilian Unified Health System (SUS), from age 20 onwards, the BMI (Basal Metabolic Rate) is the same for everyone above that age.
+    if (age >= 20)
+        age = 20;
+
+    // look for the age in the column
+    while (fgets(line, sizeof(line), table))
+    {
+        int csvAge;
+        char *token;
+
+        token = strtok(line, ",");
+        csvAge = atoi(token);
+
+        counter++;
+
+        if (csvAge == (int)age)
+        {
+            // Creates variables from the values ​​in the column
+            bmiTable.minNormal = atof(strtok(NULL, ","));
+            bmiTable.maxNormal = atof(strtok(NULL, ","));
+            bmiTable.obesityI  = atof(strtok(NULL, ","));
+            bmiTable.obesityII = atof(strtok(NULL, ","));
+
+            break;
+        }
+    }
+
+    // Close the table
+    fclose(table);
+
+    return bmiTable;
+}
+
+/**
+ * Calculates BMI and ideal weight range based on weight and height.
+ */
+double calculateBMI(double weight, double height, double minNormal, double maxNormal)
 {
     // Formula: BMI = weight / height²
     float heightSquared = height * height;
     float bmi = weight / heightSquared;
 
     // Ideal weight range based on BMI thresholds
-    float idealWeightMax = BMI_MAX_NORMAL * heightSquared;
-    float idealWeightMin = BMI_MIN_NORMAL * heightSquared;
-
-    printf("Classification: ");
-
-    if(bmi < BMI_MIN_NORMAL)
-        printf(YELLOW "Underweight\n" RESET);
-    else if(bmi < BMI_MAX_NORMAL)
-        printf(GREEN "Normal weight\n" RESET);
-    else if(bmi < BMI_MIN_OB1)
-        printf(YELLOW "Obesity I.\n");
-    else if(bmi < BMI_MIN_OB2)
-        printf(YELLOW BOLD "Obesity II.\n" RESET);
-    else
-        printf(RED "Obesity III." RESET);
+    float idealWeightMax = maxNormal * heightSquared;
+    float idealWeightMin = minNormal * heightSquared;
 
     printf("Your BMI is %.1lf\n", bmi);
 
     printf(CYAN "Ideal weight between %.1lf kg and %.1lf kg.\n" RESET, idealWeightMin, idealWeightMax);
+
+    return bmi;
+}
+/**
+ * Classify BMI based on the table.
+ */
+void classifyBMI(double bmi, double minNormal, double maxNormal, double obesityI, double obesityII)
+{
+    if (bmi < minNormal)
+        printf(YELLOW "Underweight.\n" RESET);
+    else if (bmi < maxNormal)
+        printf(GREEN "Normal weight.\n" RESET);
+    else if (bmi < obesityI)
+        printf(YELLOW "Obesity I.\n");
+    else if (bmi < obesityII)
+        printf(YELLOW BOLD "Obesity II.\n" RESET);
+    else
+        printf(RED "Obesity III." RESET);
 }
 
 /**
  * Clears the screen using system command for the current platform.
  */
-void clearScreen()
+void clearScreen(void)
 {
     // Clear the screen based on the operating system
     #if defined(_WIN32)
@@ -106,7 +184,8 @@ void clearScreen()
 /**
  * Renders the ASCII banner for the BMI app.
  */
-void printBanner() {
+void printBanner(void)
+{
     printf(CYAN BOLD
         "=====================================\n"
         "       █████╗  ███╗   ███╗██╗\n"
@@ -117,7 +196,7 @@ void printBanner() {
         "       ╚══════╝╚═╝     ╚═╝╚═╝\n"
         "=====================================\n"
         "      BODY MASS INDEX\n"
-        "=====================================\n\n"
+        "=====================================\n"
         RESET
     );
 }
